741b384cdf185512ea72b35dd9ca5ae45bca90b2
Hemant <hy564636@gmail.com>
2025-06-20 12:58:14 +0530

    created database methods for createpost,updatepost,deletepost,getpost,getallpost

--- Blog-website-react/src/Appwrite/Config.js
+++ Blog-website-react/src/Appwrite/Config.js
@@ -1,6 +1,6 @@
 import { use } from "react";
 import conf from "../Conf/Conf";
-import { Client, ID,Databases,Storage,Query } from "appwrite";
+import { Client, ID,Databases,Storage,Query, Query } from "appwrite";
 
 
 export class DBService{
@@ -12,7 +12,82 @@ export class DBService{
             .setEndpoint(conf.appwriteUrl) // Your API Endpoint
             .setProject(conf.appwriteProjetID); // Your project ID
         this.database=new Databases(this.Client);
-        this.bucket=new Storage(this.Client);
+        this.bucket=new Storage(this.Client); 
+    }
+
+    async CreatePost({title,slug,content,featuredImage,Status,UserId}){
+        try {
+            return await this.database.createDocument(
+                conf.appwriteDatabaseID, // Database ID
+                conf.appwriteCollectionID, // Collection ID
+                slug, // Document ID
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    Status,
+                    UserId
+                }
+
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+    async UpdatePost(slug,{title,content,featuredImage,Status}){
+        try {
+            return await this.database.updateDocument(
+                conf.appwriteDatabaseID, // Database ID
+                conf.appwriteCollectionID, // Collection ID
+                slug, // Document ID
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    Status
+                }
+
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async DeletePost(slug){
+        try {
+            await this.database.deleteDocument(
+                conf.appwriteDatabaseID, // Database ID
+                conf.appwriteCollectionID, // Collection ID
+                slug // Document ID
+            )
+            return true;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.database.getDocument(
+                conf.appwriteDatabaseID, // Database ID
+                conf.appwriteCollectionID, // Collection ID
+                slug // Document ID
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async getAllPosts(querries=[Query.equal('Status','active')]){
+        try {
+            return await this.database.listDocuments(
+                conf.appwriteDatabaseID, // Database ID
+                conf.appwriteCollectionID, // Collection ID
+                querries // Query array
+            )
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
