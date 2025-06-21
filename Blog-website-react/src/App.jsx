dda8b68234c2e662cde7e02fece1b71b2bf3adf3
Hemant <hy564636@gmail.com>
2025-06-21 21:45:36 +0530

    commiting

--- Blog-website-react/src/App.jsx
+++ Blog-website-react/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import { useDispach } from 'react-redux'
 import './App.css'
+import authService from './services/authService'
+import{login,logout} from './store/authSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch=useDispach()
+  useEffect(()=>{
+    authService.GetCurrentUser()
+    .then((userData)=>{
+      if(userData){
+      dispatch(login({UserData:userData}))
+    }
+    })
+  },[])
   return (
     <>
       <h1>Hello world</h1>
